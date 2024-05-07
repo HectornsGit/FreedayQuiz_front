0dff9887dd6b8052d82b3789e55008557601202c
lidiapdiaz <lidiapd15@hotmail.com>
2024-05-07 21:03:25 +0200

    add logo for mobile and pc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="flex flex-col items-center justify-between bg-pink-100 p-8">
+        <header className="flex items-center justify-between bg-pink-100 p-8">
             <h1>
-                <Image src='/logo-HAB-mobile.png' alt='Logo hack a boss' width={150} height={59}/>
+                {/* Pongo las imágenes asi porque cuando es versión movil aparece un logo y cuando es version mas grande, otro */}
+                <Image src="/logo-HAB-mobile.png" alt="Logo hack a boss" width={150} height={59} className="block md:hidden"/>
+                <Image src="/logo-HAB-pc.png" alt="Logo hack a boss" width={100} height={85} className="hidden md:block"/>
             </h1>
             <nav>
                 <ul>
