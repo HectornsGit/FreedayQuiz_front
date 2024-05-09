c6e455c007bebd9c4d34233e95b608304a81ca5a
lidiapdiaz <lidiapd15@hotmail.com>
2024-05-09 20:44:05 +0200

    fix animation for burguer menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,13 +19,13 @@ export default function Header() {
         <header className="flex items-center justify-between text-white p-8 bg-header">
             <h1>
                 {/* Pongo las imágenes asi porque cuando es versión movil aparece un logo y cuando es version mas grande, otro */}
-                <Image src="/logo-HAB-mobile.png" alt="Logo hack a boss" width={150} height={59} className="block md:hidden"/>
+                <Image src="/logo-HAB-mobile.png" alt="Logo hack a boss" width={100} height={39} className="block md:hidden"/>
                 <Image src="/logo-HAB-pc.png" alt="Logo hack a boss" width={100} height={85} className="hidden md:block"/>
             </h1>
     <nav>
-        <button onClick={toggleMenu} className="block md:hidden">
+        <button onClick={toggleMenu} className="md:hidden">
             <svg className="icon-menu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
             </svg>
         </button>
         <ul className={`burguer-menu ${menu ? 'activated' : ''}`}>
