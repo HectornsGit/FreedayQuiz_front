990ffee4647a2aeff418896ed4d81bd48db07921
Vlad Beltran <puentes.studio@gmail.com>
2024-05-09 18:58:13 +0100

    login page created

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+function RegisterPage() {
+  return (
+    <div className="h-dvh flex flex-col justify-center items-center">
+      <h1>Log In</h1>
+    </div>
+  );
+}
+
+export default RegisterPage;
