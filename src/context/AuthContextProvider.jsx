f9d2992be9cf14327851ff64b0d451934538857e
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-05-31 16:26:48 +0200

    Repair uselocalstorage and testing in a example page

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,15 +1,15 @@
-'use client';
-import { createContext } from 'react';
-import { useLocalStorage } from '@/hooks/useLocalStorage';
+'use client'
+import { createContext } from 'react'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
-    const [token, setToken] = useLocalStorage('token', null);
+    const [token, setToken] = useLocalStorage('token', null)
 
     return (
-    <AuthContext.Provider value={{ token, setToken}}> 
-      {children}
-    </AuthContext.Provider>
-    );
-};
+        <AuthContext.Provider value={{ token, setToken }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
