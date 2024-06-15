326a11116bfa5dcca007d48f2490835abfceb8ac
lidiapdiaz <lidiapd15@hotmail.com>
2024-06-15 19:54:30 +0200

    feat when click in the ListQuestions´s img go to their path to edit question

--- src/components/ListQuestions.jsx
+++ src/components/ListQuestions.jsx
@@ -8,23 +8,31 @@ import { fetchAPI } from '@/api/fetch-api'
 //Con el estado puedo 'setear' las imagenes que me llegan del back :)
 import { useState } from 'react'
 
-import Slider from './Slider' //importamos slider para mover imgs de izquierda a derecha
+//importamos slider para mover imgs de izquierda a derecha
+import Slider from './Slider' 
+
+//importamos unseRouter para usar enrutados
+import { useRouter } from 'next/navigation' 
 
 
 export default function ListQuestions() {
     const url = process.env.NEXT_PUBLIC_API_HOST //para indicar mas facil la ruta dónde estan las imágenes
     const { data: session } = useSession() //para obtener el token de la sesion.
     const [imgPreview, setImgPreview] = useState([]) //iniciamos con un array vacío para poder hacer un .map
-    const [modal, setModal]  = useState(false) // para activar un modal
+    const [modal, setModal]  = useState(false) // para activar un modal (se abre cuando haces click al boton)
+
+    const router = useRouter() //llamo al router para hacer enrutado (para llevar a la ruta de las questions)
     
     const handleUpdate = async (session) => {
         //funcion que se ejecuta cuando va todo bien :)
         const onSuccess = async (_data) => {
-        const questions = _data[0].quizzes[0].questions; //esto son las preguntas del quizz
-        const images = questions.map(question => question.image); //hago un map de las preguntas para traerme las img en un array
-        setImgPreview(images); //las seteo en un estado
+            const questions = _data[0].quizzes[0].questions; //de toda la data(JSON) del back, cojo la parte de las preguntas
+            const dataQuestions = questions.map(question => ({
+                image: question.image,
+                id: question.id
+            })); //me creo un objeto, dataQuestions con los datos que me interesan (imagen e id)
+        setImgPreview(dataQuestions); //para setear los datos e imprimir lo que corresponda
         setModal(!modal); //abre las imgs en un modal
-        console.log('EXITOOO, aqui tienes las preguntas', images)
         }
     
         //funcion que se ejecuta cuando hay algun error
@@ -51,6 +59,10 @@ export default function ListQuestions() {
         }
     };
 
+    //funcion para que cargue ruta dinámica, al hacer click en la imagen nos lleva a editar la pregunta que toque (id params)
+    const handleRoute =  (id) => {
+        router.push(`/preguntas/${id}`) //lleva a la ruta con la id de la pregunta TODO !! esto es una ruta de ejmplo
+    }
 
 
     return ( 
@@ -58,9 +70,15 @@ export default function ListQuestions() {
         <button className='p-3 bg-[--cyan] text-black'  onClick={() => handleUpdate(session)}> Lista las preguntas</button>
         {modal &&
         <Slider>
-            {imgPreview.length > 0 ? (
-                imgPreview.map((imgQuestion, index) => (
-                    <img key={index} src={`${url}/uploads/${imgQuestion}`} alt={`foto portada de la pregunta ${index}`} className='w-[300px] inline-block p-2 cursor-pointer'/>
+            {imgPreview ? (
+                imgPreview.map((question, index) => (
+                    <img 
+                    onClick={() => handleRoute(question.id)}
+                    key={index} 
+                    src={`${url}/uploads/${question.image}`} 
+                    alt={`foto portada de la pregunta ${question.id}`} 
+                    className='w-[300px] inline-block p-2 cursor-pointer'
+                    />
             ))
             ):
                 (''
