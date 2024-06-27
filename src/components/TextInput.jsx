12aefcf679f3287393e73a422f10b0b7a7a0b1aa
HectornsGit <hector.novoa.segade@gmail.com>
2024-06-27 13:07:25 +0200

    feat: changes the button to trigger text area for a prop named isArea

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -2,27 +2,19 @@
 
 import { useState } from 'react'
 
-const TextInput = ({ id, text }) => {
+const TextInput = ({ id, text, isArea=false }) => {
     const [data, setData] = useState()
-    const [isExpanded, setIsExpanded] = useState(false)
     return (
         <div className="flex w-60 my-6  flex-col items-start">
             <label
-                className="mx-8 position relative inset-y-4 bg-black px-2 text-md "
+                className="mx-8  bg-black px-2 text-md "
                 htmlFor={id}
             >
                 {text}
             </label>
-            <button
-                className="bg-blue-300 m-0 p-0 self-end w-4 h-4 z-30 relative top-7 left-4"
-                onClick={(e) => {
-                    e.preventDefault()
-                    setIsExpanded(!isExpanded)
-                }}
-            ></button>
-            {isExpanded ? (
+            {isArea ? (
                 <textarea
-                    className=" mx-8 font-semibold z-10 p-2 min-h-12 pr-6  text-black text-md py-2"
+                    className=" mx-8 font-semibold p-2 min-h-12 pr-6  text-black text-md py-2"
                     type="text"
                     id={id}
                     value={data}
