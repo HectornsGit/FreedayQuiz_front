2320b789ae751945baa3a81d83b6b20ffa427a7a
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-04 16:14:09 +0200

    feat: adapts the TextInput to work with the socket logic made

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,6 @@
 'use client'
 
-import { useState } from 'react'
-
-const TextInput = ({ id, text, isArea = false }) => {
-    const [data, setData] = useState()
+const TextInput = ({ id, text, isArea = false, value, handleChange }) => {
     return (
         <div className="flex w-60 my-6 flex-col items-start">
             <label className=" bg-black px-2 text-md " htmlFor={id}>
@@ -14,16 +11,18 @@ const TextInput = ({ id, text, isArea = false }) => {
                     className=" font-semibold p-2 min-h-12 pr-6  text-black text-md py-2"
                     type="text"
                     id={id}
-                    value={data}
-                    onChange={(e) => setData(e.target.value)}
+                    name={id}
+                    value={value}
+                    onChange={handleChange}
                 />
             ) : (
                 <input
-                    className="mx-8 font-semibold  z-10 p-2 pr-6 text-black text-md py-2"
+                    className="font-semibold  z-10 p-2 pr-6 text-black text-md py-2"
                     type="text"
                     id={id}
-                    value={data}
-                    onChange={(e) => setData(e.target.value)}
+                    name={id}
+                    value={value}
+                    onChange={handleChange}
                 />
             )}
         </div>
