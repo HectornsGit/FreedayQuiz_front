1448098972e9f745aa1e6b4f09139a5b84d180a3
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-05 13:35:27 +0200

    feat: updates the css

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -2,13 +2,13 @@
 
 const TextInput = ({ id, text, isArea = false, value, handleChange }) => {
     return (
-        <div className="flex w-60 my-6 flex-col items-start">
+        <div className="flex w-60 my-6 flex-col items-start m-2">
             <label className=" bg-black px-2 text-md " htmlFor={id}>
                 {text}
             </label>
             {isArea ? (
                 <textarea
-                    className=" font-semibold p-2 min-h-12 pr-6  text-black text-md py-2"
+                    className="col-span-2 font-semibold p-2 min-h-20 min-w-56   text-black text-sm py-2"
                     type="text"
                     id={id}
                     name={id}
@@ -17,7 +17,7 @@ const TextInput = ({ id, text, isArea = false, value, handleChange }) => {
                 />
             ) : (
                 <input
-                    className="font-semibold  z-10 p-2 pr-6 text-black text-md py-2"
+                    className="font-semibold  p-2  text-black text-md py-2"
                     type="text"
                     id={id}
                     name={id}
