d862feed31a69903d5372dfaf1384a21a27b8f3e
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-12 12:50:43 +0200

    feat: updates styles and functionality acording to figma designs

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,29 +1,51 @@
 'use client'
 
-const TextInput = ({ id, text, isArea = false, value, handleChange }) => {
+import { useState } from 'react'
+import YellowBgSendPlane from './icons/yellowBgSendPlane'
+import YellowPencil from './icons/YellowPencil'
+
+const TextInput = ({ id, text, value, handleChange }) => {
+    const [isInput, setIsInput] = useState(false)
+    const handleChangeInput = (e) => {
+        e.preventDefault()
+        setIsInput(!isInput)
+    }
     return (
         <div className="flex w-60 my-6 flex-col items-start m-2">
-            <label className=" bg-black px-2 text-md " htmlFor={id}>
+            <label className="text-md my-3 font-medium" htmlFor={id}>
                 {text}
             </label>
-            {isArea ? (
-                <textarea
-                    className="col-span-2 font-semibold p-2 min-h-20 min-w-56   text-black text-sm py-2"
-                    type="text"
-                    id={id}
-                    name={id}
-                    value={value}
-                    onChange={handleChange}
-                />
+            {isInput ? (
+                <ul className="flex">
+                    <li>
+                        <input
+                            className="font-semibold  p-2  text-black text-md py-2"
+                            type="text"
+                            id={id}
+                            name={id}
+                            value={value}
+                            onChange={handleChange}
+                        />
+                    </li>
+                    <li>
+                        <button onClick={handleChangeInput}>
+                            <YellowBgSendPlane
+                                className={'w-10 p-1'}
+                            ></YellowBgSendPlane>
+                        </button>
+                    </li>
+                </ul>
             ) : (
-                <input
-                    className="font-semibold  p-2  text-black text-md py-2"
-                    type="text"
-                    id={id}
-                    name={id}
-                    value={value}
-                    onChange={handleChange}
-                />
+                <ul className="flex items-center gap-2">
+                    <li>
+                        <button onClick={handleChangeInput}>
+                            <YellowPencil className={'w-4'}></YellowPencil>
+                        </button>
+                    </li>
+                    <li>
+                        <p className=" font-medium">{value}</p>
+                    </li>
+                </ul>
             )}
         </div>
     )
