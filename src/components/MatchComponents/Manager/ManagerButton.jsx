b5119c0219208d936c85ff0b4b1f72083f236d7d
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-22 20:12:55 +0200

    fix: fixes weird black lines over the component

--- src/components/MatchComponents/Manager/ManagerButton.jsx
+++ src/components/MatchComponents/Manager/ManagerButton.jsx
@@ -5,13 +5,13 @@ const ManagerButton = ({
     isPrimary = false,
 }) => {
     return (
-        <div className="bg-black md:w-64 w-48">
+        <div className="bg-black mt-4 bg-fixed disabled:bottom-1 h-12 md:w-64 w-48">
             <button
                 className={
                     (isPrimary
                         ? ' bg-gradient hover:box-shadow-yellow hover:bg-black '
                         : 'bg-white hover:bg-black hover:text-white hover:box-shadow-white ') +
-                    'text-black  font-extrabold text-lg w-full h-full mx-0  px-2 py-2 mt-5 disabled:bg-black disabled:box-shadow-gray   disabled:text-gray-600 '
+                    'text-black relative bottom-5 box-border font-extrabold text-lg w-full h-12 mx-0  px-2 py-2 mt-5 disabled:bottom-6 disabled:bg-black disabled:box-shadow-gray   disabled:text-gray-600 '
                 }
                 onClick={handleClick}
                 disabled={disabled}
