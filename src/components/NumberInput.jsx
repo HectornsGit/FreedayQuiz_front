690f3d5e9742d476bba7709426760865204ad23d
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-23 21:15:28 +0200

    fix: Fixes an error where the default value was not set correctly

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,8 +1,14 @@
 'use client';
 
-const NumberInput = ({ id, text, value, isInput, handleChange }) => {
+import { useEffect, useState } from 'react';
+
+const NumberInput = ({ id, text, defaultValue, isInput }) => {
+    const [value, setValue] = useState(defaultValue);
+    useEffect(() => {
+        setValue(defaultValue);
+    }, [defaultValue]);
     return (
-        <div className="flex font-medium  text-left my-6 flex-col items-start m-2">
+        <div className="flex md:font-medium font-light text-left my-6 flex-col items-start m-2">
             <label className="text-md " htmlFor={id}>
                 {text}
             </label>
@@ -13,10 +19,13 @@ const NumberInput = ({ id, text, value, isInput, handleChange }) => {
                     id={id}
                     name={id}
                     value={value}
-                    onChange={handleChange}
+                    onChange={(e) => {
+                        e.preventDefault();
+                        setValue(e.target.value);
+                    }}
                 />
             ) : (
-                <p className="font-semibold w-20  p-2  text-black text-md py-2">
+                <p className="font-semibold w-20  p-2  text-white text-md py-2">
                     {value}
                 </p>
             )}
