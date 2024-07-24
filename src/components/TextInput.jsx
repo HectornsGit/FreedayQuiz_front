386ac95e4dd8182f57a36d0b07637f11df507f8e
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-24 22:06:58 +0200

    fix: changes prop value to  defaultValue and fixes onChange logic

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,36 +1,28 @@
-import YellowBgSendPlane from './icons/yellowBgSendPlane';
-
-const TextInput = ({ id, text, isInput, value, handleChange }) => {
-    const handleChangeInput = (e) => {
-        e.preventDefault();
-    };
+import { useEffect, useState } from 'react';
+const TextInput = ({ id, text, isInput, defaultValue }) => {
+    const [value, setValue] = useState(defaultValue);
+    useEffect(() => {
+        setValue(defaultValue);
+    }, [defaultValue]);
     return (
         <div className="flex w-60 my-6 flex-col items-start m-2">
             <label className="text-md my-3 font-medium" htmlFor={id}>
                 {text}
             </label>
             {isInput ? (
-                <ul className="flex">
-                    <li>
-                        <input
-                            className="font-semibold  p-2  text-black text-md py-2"
-                            type="text"
-                            id={id}
-                            name={id}
-                            value={value}
-                            onChange={handleChange}
-                        />
-                    </li>
-                    <li>
-                        <button onClick={handleChangeInput}>
-                            <YellowBgSendPlane
-                                className={'w-10 p-1'}
-                            ></YellowBgSendPlane>
-                        </button>
-                    </li>
-                </ul>
+                <input
+                    className="font-semibold  p-2  text-black text-md py-2"
+                    type="text"
+                    id={id}
+                    name={id}
+                    value={value}
+                    onChange={(e) => {
+                        e.preventDefault();
+                        setValue(e.target.value);
+                    }}
+                />
             ) : (
-                <p className=" font-medium">{value}</p>
+                <p className=" font-medium">{defaultValue}</p>
             )}
         </div>
     );
