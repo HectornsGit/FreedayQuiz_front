c2dad5b17587adf37b44c28a1185948593a8c2b8
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-26 12:30:39 +0200

    feat: styles the waiting for quiz to start section.

--- src/components/MatchComponents/Player/MatchComponentAsPlayer.jsx
+++ src/components/MatchComponents/Player/MatchComponentAsPlayer.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import QuestionComponent from '../QuestionComponent';
 import ManagerButton from '../Manager/ManagerButton';
+import YellowBgSendPlane from '@/components/icons/YellowBgSendPlane';
 
 const MatchComponentAsPlayer = ({ playerProps }) => {
     const {
@@ -30,7 +31,7 @@ const MatchComponentAsPlayer = ({ playerProps }) => {
 
     if (sessionRecovery && isNameSetted) {
         return (
-            <section className="flex h-[40vh] flex-col justify-center items-center">
+            <section className="flex h-[40vh]  pt-20  flex-col justify-center items-center">
                 {quizData && (
                     <h1 className="text-xl font-bold mb-12">
                         {quizData?.title}
@@ -70,35 +71,75 @@ const MatchComponentAsPlayer = ({ playerProps }) => {
                     </div>
                 </>
             ) : (
-                <section className="flex flex-col items-center">
-                    <input
-                        type="text"
-                        placeholder="Introduce tu nombre de jugador"
-                        value={
-                            initialPlayerData[0]?.name &&
-                            initialPlayerData[0]?.name
-                        }
-                        hidden={socket?.Mydata?.name ? true : false}
-                        onChange={(e) => setNickName(e.target.value)}
-                        required
-                    />
-                    <button
-                        onClick={handleInitialPlayerData}
-                        hidden={socket?.Mydata?.name ? true : false}
-                    >
-                        Envía tu nombre de jugador
-                    </button>
+                <section className="flex flex-col w-[70vw] lg:w-5/6 sm:w-4/6  items-center">
+                    <header className="self-center">
+                        {quizData && (
+                            <h1 className="text-2xl font-bold mb-12">
+                                {quizData?.title}
+                            </h1>
+                        )}
+                    </header>
+
                     {!isQuestionRunning && (
-                        <p>Jugador: {initialPlayerData[0]?.name}</p>
-                    )}
-                    {!quizData ? (
-                        <>
-                            <p>Esperando que el master inicie el juego...</p>
-                        </>
-                    ) : (
-                        <h1 className="text-xl">{quizData?.title}</h1>
+                        <div className="self-center flex flex-col  items-center mb-8">
+                            <p className="font-semibold text-md">
+                                Esperando que el master inicie el quiz...
+                            </p>
+                            <ul className="flex w-full justify-center my-8 gap-4">
+                                <li>
+                                    <div className=" bg-[--yellow] rounded-full w-8 h-8"></div>
+                                </li>
+                                <li>
+                                    <div className='className=" bg-[--cyan] rounded-full w-8 h-8'></div>
+                                </li>
+                                <li>
+                                    <div className="bg-[--yellow] rounded-full w-8 h-8"></div>
+                                </li>
+                            </ul>
+                        </div>
                     )}
 
+                    {!isQuestionRunning && !showScores && (
+                        <p className="self-start font-medium mb-10">
+                            Jugadores conectados: {connectedClients}
+                        </p>
+                    )}
+                    <form className="flex self-start items-end">
+                        <div className="flex flex-col items-start">
+                            <label
+                                hidden={socket?.Mydata?.name ? true : false}
+                                htmlFor="player"
+                            >
+                                Nombre
+                            </label>
+                            <input
+                                id="player"
+                                type="text"
+                                className="font-semibold  p-2  text-black text-md py-1"
+                                value={
+                                    initialPlayerData[0]?.name &&
+                                    initialPlayerData[0]?.name
+                                }
+                                hidden={socket?.Mydata?.name ? true : false}
+                                onChange={(e) => setNickName(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button
+                            type="button"
+                            onClick={handleInitialPlayerData}
+                            hidden={socket?.Mydata?.name ? true : false}
+                        >
+                            <YellowBgSendPlane
+                                className={'w-8'}
+                            ></YellowBgSendPlane>
+                        </button>
+                    </form>
+                    {!isQuestionRunning && initialPlayerData[0]?.name && (
+                        <p className="bg-[--yellow] text-lg font-bold w-36 px-4 self-start text-black text-md py-2 ">
+                            {initialPlayerData[0]?.name}
+                        </p>
+                    )}
                     {question && isQuestionRunning && (
                         <section className="flex flex-col  items-center lg:w-5/6 w-screen ">
                             <QuestionComponent
