670547f60c2880dd47c41810ea4fb6664222efa3
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-07-27 17:35:45 +0200

    Remove the previously removed question from the dropdown list

--- src/quizEngine/handleEvents/questionDeletedHandler.js
+++ src/quizEngine/handleEvents/questionDeletedHandler.js
@@ -1,12 +1,30 @@
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
 
-const questionDeletedHandler = (question, quizData, socket, quizId) => {
+const questionDeletedHandler = (
+    question,
+    quizData,
+    socket,
+    quizId,
+    setQuizData
+) => {
     if (socket) {
-        socket.on('questionDeleted', (message) => {
-            toast.success(message.message)
+        socket.on('questionDeleted', (data) => {
+            toast.success(data.message);
 
-            const nextQuestion = question?.questionNumber + 1
-            const numberOfQuestions = quizData.number_of_questions
+            //Lógica para actualizar la lista desplegable de preguntas:
+            const newListOfQuestions = quizData.list_of_questions.filter(
+                (question) => question.number !== data.questionNumber
+            );
+            const newQuizData = {
+                ...quizData,
+                list_of_questions: newListOfQuestions,
+                number_of_questions: newListOfQuestions.length,
+            };
+            setQuizData(newQuizData);
+
+            //Ahora preparamos los datos para actualizar la siguiente pregunta:
+            const nextQuestion = question?.questionNumber + 1;
+            const numberOfQuestions = quizData.number_of_questions;
 
             if (socket && question && quizId) {
                 socket.emit(
@@ -14,11 +32,11 @@ const questionDeletedHandler = (question, quizData, socket, quizId) => {
                     quizId,
                     nextQuestion,
                     numberOfQuestions
-                )
+                );
             } else {
-                toast.warning('Esta era la última pregunta.')
+                toast.warning('Esta era la última pregunta.');
             }
-        })
+        });
     }
-}
-export default questionDeletedHandler
+};
+export default questionDeletedHandler;
