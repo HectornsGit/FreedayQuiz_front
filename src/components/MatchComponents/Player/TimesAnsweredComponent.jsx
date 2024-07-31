e099684d3aab8b54671032f1ae9427d5263962eb
HectornsGit <hector.novoa.segade@gmail.com>
2024-07-31 19:57:59 +0200

    feat: adds a component to show the times an answered was picked

---
+++ src/components/MatchComponents/Player/TimesAnsweredComponent.jsx
@@ -0,0 +1,17 @@
+import PersonLogo from '@/components/icons/PersonLogo';
+
+const TimesAnsweredComponent = ({ clickedResponses, response }) => {
+    return (
+        <div className=" m-0 relative top-6 sm:left-5 left-40 gap-1 bg-black text-[--yellow] flex items-center  p-2 w-10 h-10 rounded-full">
+            <span>
+                <PersonLogo className={'pt-1 w-2 fill-[--yellow]'}></PersonLogo>
+            </span>
+            <span>
+                {(clickedResponses[Object.values(response)[0]] &&
+                    clickedResponses[Object.values(response)[0]]) ||
+                    0}
+            </span>
+        </div>
+    );
+};
+export default TimesAnsweredComponent;
