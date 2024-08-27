d9b3deb3d735fff0a9b0c9da4becaa7350221434
Javier <javierhuertastrg@outlook.com>
2024-08-27 13:39:19 +0200

    cambio de logos

--- src/components/ButtonSecondary.jsx
+++ src/components/ButtonSecondary.jsx
@@ -4,7 +4,7 @@ export default function ButtonSecondary({ href, children }) {
     return (
         <Link
             href={href}
-            className="text-black font-extrabold text-lg bg-white px-5 py-2hover:bg-black transition-colors duration-300 ease-in-out hover:text-white hover:box-shadow-white"
+            className="text-black font-extrabold text-lg bg-white px-5 py-2 hover:bg-black transition-colors duration-300 ease-in-out hover:text-white hover:box-shadow-white"
         >
             {children}
         </Link>
