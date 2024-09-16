9bc1d98c7687e8106982d220838a720d6d4c0461
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-09-16 18:06:10 +0200

    Arreglar error de refrescar la página

--- src/middlewares/RoutesAuth.jsx
+++ src/middlewares/RoutesAuth.jsx
@@ -4,12 +4,19 @@ import React from 'react';
 import { useSession } from 'next-auth/react';
 
 export default function RoutesAuth({ children }) {
-    const { data: session } = useSession();
+    const { data: session, status } = useSession();
     const router = useRouter();
+
     React.useEffect(() => {
+        if (status === 'loading') return;
         if (!session) {
             router.push('/login');
         }
-    }, [session, router]);
+    }, [session, status, router]);
+
+    if (status === 'loading') {
+        return <p>Loading...</p>;
+    }
+
     return session ? children : null;
 }
