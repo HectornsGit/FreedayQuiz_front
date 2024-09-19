8914f0c3e13c28c7cb29d1d9399431b65b2f6a69
HectornsGit <hector.novoa.segade@gmail.com>
2024-09-19 16:25:45 +0200

    feat: now displays players ordered by score

--- src/components/MatchComponents/ListPlayerStats.jsx
+++ src/components/MatchComponents/ListPlayerStats.jsx
@@ -3,11 +3,20 @@ const ListPlayerStats = ({ players }) => {
     if (players.length > 0)
         return (
             <ul className="text-xl px-2 mb-8">
-                {players.map((player) => (
-                    <li key={player.id} className="border-b flex  justify-end">
-                        <PlayerStats player={player}></PlayerStats>
-                    </li>
-                ))}
+                {players
+                    .sort((playerA, playerB) => {
+                        if (playerA.totalScore < playerB.totalScore) return +1;
+                        if (playerA.totalScore > playerB.totalScore) return -1;
+                        else return 0;
+                    })
+                    .map((player) => (
+                        <li
+                            key={player.id}
+                            className="border-b flex  justify-end"
+                        >
+                            <PlayerStats player={player}></PlayerStats>
+                        </li>
+                    ))}
             </ul>
         );
     else return <p>No hay jugadores conectados</p>;
