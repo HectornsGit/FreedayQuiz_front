da0c6588a7fd7bba0bad4399f821ee5004f9c781
lidiapdiaz <lidiapd15@hotmail.com>
2024-09-21 14:54:11 +0200

    fix overflow in ResultsAnswers component

--- src/components/MatchComponents/Player/ResultAnswers .jsx
+++ src/components/MatchComponents/Player/ResultAnswers .jsx
@@ -16,9 +16,9 @@ const ResultAnswers = ({ question, initialPlayerData, timeLeft }) => {
                     }
                 >
                     <StarEyedSmiley className={'w-[100px] my-8'} />
-                    <div className="h-[180px] overflow-hidden">
-                        <div className="bg-[#2be002] w-[700px] h-[700px] rounded-t-full">
-                            <h2 className="text-black font-black xl:text-3xl text-2xl w-fit relative top-[7rem] xl:left-[12rem] left-[14rem]">
+                    <div className="flex justify-center w-full lg:h-40 md:h-40 h-36 overflow-hidden">
+                        <div className="lg:w-[800px] md:w-[400px] w-[320px] lg:h-96 md:h-48 h-40 bg-[#2be002] rounded-t-full">
+                            <h2 className="inline-block mt-20 text-center text-black font-black xl:text-3xl text-2xl">
                                 ¡Respuesta correcta!
                             </h2>
                         </div>
@@ -35,9 +35,9 @@ const ResultAnswers = ({ question, initialPlayerData, timeLeft }) => {
                     }
                 >
                     <Skull className={'w-[100px] my-8 fill-[#ff3284]'} />
-                    <div className="h-[180px] overflow-hidden">
-                        <div className="bg-[#ff3284] w-[700px] h-[700px] rounded-t-full">
-                            <h2 className="text-black font-black xl:text-3xl text-2xl w-fit relative top-[7rem] xl:left-[12rem] left-[14rem]">
+                    <div className="flex justify-center w-full lg:h-40 md:h-40 h-36 overflow-hidden">
+                        <div className="lg:w-[800px] md:w-[400px] w-[320px] lg:h-96 md:h-48 h-40 bg-[#ff3284] rounded-t-full">
+                            <h2 className="inline-block mt-20 text-center text-black font-black xl:text-3xl text-2xl">
                                 ¡Respuesta incorrecta!
                             </h2>
                         </div>
