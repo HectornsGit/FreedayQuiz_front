7f19933e4c2b56d30da4611f616d7a451c380823
HectornsGit <hector.novoa.segade@gmail.com>
2024-09-21 23:15:03 +0200

    feat: updates the component to match new necesities

--- src/components/MatchComponents/Manager/ListAnswerInputs.jsx
+++ src/components/MatchComponents/Manager/ListAnswerInputs.jsx
@@ -1,24 +1,35 @@
-import TextInput from '@/components/TextInput'
-const ListAnswerInputs = ({ answerPropsList }) => {
+import AnswerInputComponent from './AnswerInputComponent.jsx';
+import React, { useEffect, useState } from 'react';
+
+const ListAnswerInputs = ({ answers }) => {
+    const answerNames = ['🌞', '🌜', '🌟', '⚡'];
+
+    const [correctAnswer, setCorrectAnswer] = useState(
+        answers.filter((item) => {
+            return item.isCorrect === true;
+        })[0].key
+    );
     return (
-        <ul className="col-span-2  grid grid-cols-2 grid-rows-2 ">
-            {answerPropsList.length > 0 &&
-                answerPropsList.map((answerProps) => {
-                    const { text, id, value, handleQuestionChange } =
-                        answerProps
+        <ul className="flex flex-col  w-full items-center  gap-6">
+            {answers.length > 0 &&
+                answers.map((answer, index) => {
+                    const { key, value, isCorrect } = answer;
                     return (
-                        <li key={id}>
-                            <TextInput
-                                text={text}
-                                id={id}
-                                name={id}
-                                value={value}
-                                handleChange={handleQuestionChange}
-                            ></TextInput>
+                        <li
+                            key={key}
+                            className="xl:place-self-end p-[3PX] md:w-80  sm:w-96 w-[95vw] bg-gradient-to-r   flex items-center    from-[--yellow] from-9% via-green-400 via-50% to-[--cyan] to-94%"
+                        >
+                            <AnswerInputComponent
+                                answer={key}
+                                setCorrectAnswer={setCorrectAnswer}
+                                correctAnswer={correctAnswer}
+                                text={value}
+                                logo={answerNames[index]}
+                            ></AnswerInputComponent>
                         </li>
-                    )
+                    );
                 })}
         </ul>
-    )
-}
-export default ListAnswerInputs
+    );
+};
+export default ListAnswerInputs;
