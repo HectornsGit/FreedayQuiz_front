392e1f37c8a36b75019f27901dbd8ccf56047fdc
HectornsGit <hector.novoa.segade@gmail.com>
2024-09-21 23:15:38 +0200

    feat: adds a component to show answers without functionalities

---
+++ src/components/MatchComponents/Manager/AnswerManagerComponent.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import CheckMark from '@/components/icons/checkmark';
+
+const AnswerManagerComponent = ({ text, isCorrect, logo }) => {
+    return (
+        <div className=" flex items-center h-full gap-6 text-start text-2xl p-4  bg-black w-full ">
+            <span className="font-bold">{logo}</span>
+            <p
+                className={
+                    (text.length > 10
+                        ? ' md:text-md text-sm'
+                        : 'lg:text-2xl md:text-xl text-base ') +
+                    ' grow font-semibold'
+                }
+            >
+                {text}
+            </p>
+            {isCorrect && <CheckMark className={'h-8 fill-white '}></CheckMark>}
+        </div>
+    );
+};
+export default AnswerManagerComponent;
