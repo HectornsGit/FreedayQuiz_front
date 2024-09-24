5b8af7d39b67cd020c257cb84c87f48b6ae22f67
lidiapdiaz <lidiapd15@hotmail.com>
2024-09-24 22:43:41 +0200

    add how to play contain

--- src/components/HowToPlayManager.jsx
+++ src/components/HowToPlayManager.jsx
@@ -16,8 +16,11 @@ export default function HowToPlayManager() {
         setStep((prevStep) => prevStep + 1);
     };
 
+    {
+        /* <article className="p-4 flex gap-x-8 w-fit lg:w-max"> */
+    }
     return (
-        <article className="p-4 flex gap-x-8 w-max">
+        <article className="flex flex-row">
             <button onClick={handlePrevStep}>
                 <div className="bg-[#FCFF00] rounded-full p-2">
                     <ChevronLeft className="w-4 h-4 sm:w-5 sm:h-5 text-black" />
@@ -25,7 +28,7 @@ export default function HowToPlayManager() {
             </button>
             {/* PASO 1: Registro*/}
             {step === 1 && (
-                <div className="flex-auto">
+                <div className="flex flex-col px-7 lg:w-max w-fit">
                     <div className="flex items-center gap-x-2">
                         <p className="font-black text-black text-lg text-center bg-[--yellow] p-2 w-8">
                             1
@@ -34,11 +37,16 @@ export default function HowToPlayManager() {
                             Regístrate para crear quizes
                         </h2>
                     </div>
-                    <div>Instrucciones para creador/a de juego</div>
+                    <p>Instrucciones para creador/a de juego</p>
                     <p>
                         Despues de la explicacion, vendría un gif o video que se
                         ve dónde le clica y lo que pasa
                     </p>
+                    <img
+                        src="/registro.gif"
+                        alt="video de como registrar un usuario"
+                        className="pb-2 pr-2 bg-gradient-to-bl from-[--cyan] to-[--yellow]"
+                    />
                 </div>
             )}
             {/* PASO 2: Iniciar sesion*/}
