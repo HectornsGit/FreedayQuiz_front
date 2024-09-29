19aa7d753549cbb708e7937107838cbeb2047fa4
lidiapdiaz <lidiapd15@hotmail.com>
2024-09-29 18:13:18 +0200

    add games name and link in landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,32 +1,53 @@
 'use client';
 import useLanding from '@/hooks/useLanding';
+import Link from 'next/link';
 
 const Landing = () => {
     const { accessCode, setAccessCode, handleSubmit } = useLanding();
     return (
-        <div className="min-h-screen flex items-center justify-center bg-[var(--bg-hab-black)]">
-            <div className="text-center text-white">
-                <h1 className="text-4xl font-bold mb-4">¡Bienvenido/a!</h1>
-                <p className="mb-6">
-                    Introduce el pin de juego para empezar a jugar
-                </p>
-                <div className="flex flex-col items-center">
-                    <input
-                        type="text"
-                        placeholder="Pin de juego"
-                        className="mb-4 p-2 text-black rounded"
-                        value={accessCode}
-                        onChange={(e) => setAccessCode(e.target.value)}
-                    />
-                    <button
-                        className="text-black font-extrabold text-lg bg-gradient px-11 py-2 hover:bg-black hover:box-shadow-yellow"
-                        onClick={handleSubmit}
-                    >
-                        <p className="gradient-text">Ingresar</p>
-                    </button>
+        <>
+            <div className="min-h-screen flex flex-col items-center justify-center bg-[var(--bg-hab-black)]">
+                <div className=" text-center -mt-48 mb-10">
+                    <h2 className="text-[6rem] font-freedayquiz bg-gradient-to-r from-[--cyan] to-[--yellow] inline-block text-transparent bg-clip-text">
+                        FREE DAY
+                    </h2>
+                    <br></br>
+                    <h2 className="-mt-20 text-[9.8rem] font-freedayquiz bg-gradient-to-r from-[--cyan] to-[--yellow] inline-block text-transparent bg-clip-text">
+                        QUIZ!
+                    </h2>
+                </div>
+                <div className="text-center text-white">
+                    <h1 className="text-5xl font-bold mb-4">¡Bienvenido/a!</h1>
+                    <p className="mb-6">
+                        Introduce el pin de juego para empezar a jugar
+                    </p>
+                    <div className="flex flex-col items-center">
+                        <input
+                            type="text"
+                            placeholder="Pin de juego"
+                            className="mb-4 p-2 text-black rounded"
+                            value={accessCode}
+                            onChange={(e) => setAccessCode(e.target.value)}
+                        />
+                        <button
+                            className="text-black font-extrabold text-lg bg-gradient px-[4.2rem] py-2 hover:bg-black hover:box-shadow-yellow"
+                            onClick={handleSubmit}
+                        >
+                            <p className="gradient-text">Ingresar</p>
+                        </button>
+                        <span className="mt-6 text-xl">
+                            👾{' '}
+                            <Link
+                                href="/how-to-play"
+                                className=" mt-2 font-medium text-base underline hover:text-[--yellow]"
+                            >
+                                ¡Enséñame cómo se juega!
+                            </Link>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
